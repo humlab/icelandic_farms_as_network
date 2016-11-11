524cacce335318330151cabf630f15e22ef32e18
johanvonboer <johanvonboer@runbox.com>
2016-11-11 13:46:34 +0100

    Cleaned up and restructured the code

--- client/js/Map.class.js
+++ client/js/Map.class.js
@@ -1,69 +1,47 @@
 
 class Map {
 
-	constructor() {
-
+	constructor(containerDomId) {
+		if(containerDomId.length > 0) {
+			this.createMapInContainer(containerDomId);
+		}
 	}
 
-	createMapInContainer(container_dom_id) {
-		var dare_tile_layer = L.tileLayer('http://pelagios.dme.ait.ac.at/tilesets/imperium/{z}/{x}/{y}.png', {
-		    attribution: '&copy; <a href="http://dare.ht.lu.se/">Digital Atlas of the Roman Empire</a>.',
-			minZoom: 5,
-			maxZoom: 11
-		});
-		
+	createMapInContainer(containerDomId) {
 
-		var awmc_tile_layer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
-		    attribution: '&copy; <a href="http://awmc.unc.edu/">Ancient World Mapping Center</a>'
+		var view = new ol.View({
+		    center: ol.proj.transform([12.4852, 41.8922], 'EPSG:4326', 'EPSG:3857'),
+		    zoom: 5,
+		    minZoom: 3,
+			maxZoom: 12
 		});
 
+		//Tile layers
+		var layers = [
+		new ol.layer.Tile({
+				source: new ol.source.XYZ({
+					logo: "http://dare.ht.lu.se/pics/logo-lu-en.png",
+					url: "http://pelagios.dme.ait.ac.at/tilesets/imperium/{z}/{x}/{y}.png",
+					attributions: [
+				    new ol.Attribution({
+				      html: 'Map graciously provided by ' +
+				          '<a href="http://dare.ht.lu.se/">Digital Atlas of the Roman Empire</a> under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA</a> license.'
+				    })
+				  ]
+					})
+			}),
+			new ol.layer.Tile({
+		      source: new ol.source.OSM()
+		    })
+		];
 
-		var map = L.map(container_dom_id, {
-			layers: [dare_tile_layer, awmc_tile_layer]
-		}).setView([51.505, -0.09], 3);
-		
-
-		var baseMaps = {
-		    "DARE Map": dare_tile_layer,
-		    "AWMC Map": awmc_tile_layer
-		};
-
-		L.control.layers(baseMaps).addTo(map);
-
-		map.on('baselayerchange', function(e) {
-			console.log("hello");
+		//Create map with tile layers and view
+		this.map = new ol.Map({
+			layers: layers,
+			target: containerDomId,
+			view: view
 		});
 
-		self.map = map;
-		return map;
+		return this.map;
 	}
-
-	populateMapWithVisualizations() {
-		$.get("/service/visualizations", "", function(data) {
-			//console.log(data);
-
-			var layer = new L.geoJson(data, {
-				onEachFeature: function(feature, layer) {
-					var popupContent = ucfirst(feature.properties.siteType)+" site<br >";
-					var gazetteerUrl = getGazetteerInterfaceByName(feature.properties.gazetteer).getPlaceURL(feature.properties.gazetteerId);
-					popupContent += "<a target=\"_blank\" href=\""+gazetteerUrl+"\">"+feature.properties.gazetteer+" "+feature.properties.gazetteerId+"</a>";
-					layer.bindPopup(popupContent);
-					//layer.addTo(self.map);
-				}
-			});
-			var myIcon = L.icon({
-			    iconUrl: 'my-icon.png',
-			    iconSize: [38, 95],
-			    iconAnchor: [22, 94],
-			    popupAnchor: [-3, -76],
-			    shadowUrl: 'my-icon-shadow.png',
-			    shadowSize: [68, 95],
-			    shadowAnchor: [22, 94]
-			});
-			//layer.setIcon(myIcon);
-			layer.addTo(self.map);
-
-	    }, "json");
-		
-	}
-};
+}
