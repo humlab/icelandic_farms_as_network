8bdde5ba2a7aff13bac32d79a8a03e1ecf887813
Roger Mähler <roma0050@ad.umu.se>
2016-11-25 15:53:01 +0100

    Added example on how REST API result can be converted to GeoJSON

---
+++ client/js/create_features.js
@@ -0,0 +1,332 @@
+var x = [{
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[419002.42467939,383622.221614708]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [419002.42467939, 383622.221614708]
+  },
+  "farm_name": "Sk\u00e1lmholt",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-001",
+  "parish_name": "\u00d3lafsvallas\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 1,
+  "isleif_farms_id": 4
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[419735.844520117,383850.345189755]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [419735.844520117, 383850.345189755]
+  },
+  "farm_name": "Sk\u00e1lmholtshraun",
+  "farm_property_value": 10,
+  "farm_number": "\u00c1R-002",
+  "parish_name": "\u00d3lafsvallas\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 2,
+  "isleif_farms_id": 5
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[419639.229772289,382362.15562709]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [419639.229772289, 382362.15562709]
+  },
+  "farm_name": "\u00dej\u00f3tandi",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-003",
+  "parish_name": "\u00d3lafsvallas\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 3,
+  "isleif_farms_id": 6
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[417667.077765489,381326.540705231]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [417667.077765489, 381326.540705231]
+  },
+  "farm_name": "Urri\u00f0afoss",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-004",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 4,
+  "isleif_farms_id": 7
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[415920.332723075,378330.577117692]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [415920.332723075, 378330.577117692]
+  },
+  "farm_name": "Egilsta\u00f0ir",
+  "farm_property_value": 48,
+  "farm_number": "\u00c1R-005",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 5,
+  "isleif_farms_id": 8
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[415876.51077587,378435.856990045]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [415876.51077587, 378435.856990045]
+  },
+  "farm_name": "Egilsta\u00f0akot",
+  "farm_property_value": 12,
+  "farm_number": "\u00c1R-006",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 6,
+  "isleif_farms_id": 9
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[415539.735146496,374058.224363868]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [415539.735146496, 374058.224363868]
+  },
+  "farm_name": "Mj\u00f3sund  (Mj\u00f3sendi)",
+  "farm_property_value": 15,
+  "farm_number": "\u00c1R-007",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 7,
+  "isleif_farms_id": 620
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[415445.922210629,373623.954072688]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [415445.922210629, 373623.954072688]
+  },
+  "farm_name": "Fors\u00e6ti",
+  "farm_property_value": 5,
+  "farm_number": "\u00c1R-008",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 8,
+  "isleif_farms_id": 621
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[416633.412517014,372368.791334893]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [416633.412517014, 372368.791334893]
+  },
+  "farm_name": "Nes [Ferjunes]",
+  "farm_property_value": 10,
+  "farm_number": "\u00c1R-009",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Vill\u00edngaholtshreppur",
+  "row_number": 9,
+  "isleif_farms_id": 624
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[414912.481633894,370410.190277832]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [414912.481633894, 370410.190277832]
+  },
+  "farm_name": "Efri S\u00fdrl\u00e6kur",
+  "farm_property_value": 10,
+  "farm_number": "\u00c1R-010",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 10,
+  "isleif_farms_id": 625
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[414227.025317109,369950.83066828]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [414227.025317109, 369950.83066828]
+  },
+  "farm_name": "Sy\u00f0ri S\u00fdrl\u00e6kur",
+  "farm_property_value": 5,
+  "farm_number": "\u00c1R-011",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 11,
+  "isleif_farms_id": 626
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[413214.72332665,371962.290837667]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [413214.72332665, 371962.290837667]
+  },
+  "farm_name": "M\u00fdrar",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-012",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 12,
+  "isleif_farms_id": 627
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[412486.509023389,372445.554541226]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [412486.509023389, 372445.554541226]
+  },
+  "farm_name": "Gafl",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-013",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 13,
+  "isleif_farms_id": 628
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[412740.954744008,374052.799714648]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [412740.954744008, 374052.799714648]
+  },
+  "farm_name": "Sk\u00fafsl\u00e6kur",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-015",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 14,
+  "isleif_farms_id": 631
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[414518.620186061,374080.564790951]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [414518.620186061, 374080.564790951]
+  },
+  "farm_name": "Sy\u00f0ri Gr\u00f6f",
+  "farm_property_value": 15,
+  "farm_number": "\u00c1R-016",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 15,
+  "isleif_farms_id": 632
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[414152.337727677,374450.45126957]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [414152.337727677, 374450.45126957]
+  },
+  "farm_name": "Efri Gr\u00f6f",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-017",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 16,
+  "isleif_farms_id": 633
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[413905.905778147,376802.756242352]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [413905.905778147, 376802.756242352]
+  },
+  "farm_name": "Villingaholt",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-018",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 17,
+  "isleif_farms_id": 634
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[412796.962005264,377049.188191882]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [412796.962005264, 377049.188191882]
+  },
+  "farm_name": "Vatnsendi",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-019",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 18,
+  "isleif_farms_id": 635
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[412136.076322435,376259.485808162]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [412136.076322435, 376259.485808162]
+  },
+  "farm_name": "Vatnsholt",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-020",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 19,
+  "isleif_farms_id": 636
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[411103.56468268,375698.795205212]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [411103.56468268, 375698.795205212]
+  },
+  "farm_name": "Kolsholt",
+  "farm_property_value": 37,
+  "farm_number": "\u00c1R-021",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 20,
+  "isleif_farms_id": 637
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[410500.664293739,378298.150117906]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [410500.664293739, 378298.150117906]
+  },
+  "farm_name": "\u00d6nundarholt",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-024",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 21,
+  "isleif_farms_id": 640
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[408254.773117346,377390.832485548]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [408254.773117346, 377390.832485548]
+  },
+  "farm_name": "S\u00faluholt",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-025",
+  "parish_name": "Villingaholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 22,
+  "isleif_farms_id": 641
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[408002.74044169,379754.33891058]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [408002.74044169, 379754.33891058]
+  },
+  "farm_name": "Hr\u00f3arsholt",
+  "farm_property_value": 40,
+  "farm_number": "\u00c1R-028",
+  "parish_name": "Hr\u00f3arsholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 23,
+  "isleif_farms_id": 644
+}, {
+  "farm_geometry_geojson": "{\"type\":\"Point\",\"coordinates\":[411850.439290026,381176.923346501]}",
+  "farm_geometry": {
+    "type": "Point",
+    "coordinates": [411850.439290026, 381176.923346501]
+  },
+  "farm_name": "Hur\u00f0arbak",
+  "farm_property_value": null,
+  "farm_number": "\u00c1R-034",
+  "parish_name": "Hr\u00f3arsholtss\u00f3kn",
+  "shire_name": "Villingaholtshreppur",
+  "row_number": 24,
+  "isleif_farms_id": 651
+}];
+
+var features = $.map(x, function(y) {
+  return {
+          'type': 'Feature',
+          'geometry': y["farm_geometry"]
+        }
+});
+
+var geo_json ={
+    'type': 'FeatureCollection',
+    'crs': {
+      'type': 'name',
+      'properties': {
+        'name': 'EPSG:3857'
+      }
+    },
+   'features': features
+}
+console.log(geo_json);
